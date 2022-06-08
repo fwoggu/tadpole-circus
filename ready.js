e1e3aaff40e848e6a0f30840c203559a0f580d7e
fwoggu <107118436+fwoggu@users.noreply.github.com>
2022-06-08 11:16:07 -0400

    Create ready.js

---
+++ ready.js
@@ -0,0 +1,7 @@
+echo "# tadpole-circus" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/fwoggu/tadpole-circus.git
+git push -u origin main
